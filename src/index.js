096785d4ceee1fa940d1b3323cf34a0f1775f113
Victor <victor.parodi.montero@gmail.com>
2018-02-25 20:52:27 -0500

    Event Handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCKH7fJv9fErTVxWYNchJvW66I3lQBqpB0';
+
 const App = () => {
-  return <h1> Hi there! </h1>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
