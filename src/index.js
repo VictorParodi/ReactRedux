7f656362a77127ca4f0e73921369981cf8629a23
Victor <victor.parodi.montero@gmail.com>
2018-03-13 22:28:12 -0500

    Structure for video detail

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCKH7fJv9fErTVxWYNchJvW66I3lQBqpB0';
 
@@ -12,7 +13,11 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    // YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    //   this.setState({ videos });
+    // });
+
+    YTSearch({key: API_KEY, term: 'meteor js'}, (videos) => {
       this.setState({ videos });
     });
   }
@@ -21,6 +26,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
